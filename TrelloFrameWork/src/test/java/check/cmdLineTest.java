dc1bcb1fb8e5c428a277c524899dd5c884737636
abhay-1994 <abhay3011994@gmail.com>
2024-04-19 17:38:00 +0530

    1st

---
+++ TrelloFrameWork/src/test/java/check/cmdLineTest.java
@@ -0,0 +1,13 @@
+package check;
+
+public class cmdLineTest {
+
+//	public void mavenTest() {
+//		WebDriver driver=null;
+//		String BROWSER = System.getProperty("browser");
+//		String URL = System.getProperty("url");
+//		if(BROWSER.equals("chrome")) {
+//			driver=new ChromeDriver();
+//		}
+//		driver.get(URL);
+	}
